45df789045324b926c6f042e2723491799cd961b
Daniel Ashik Devadhas <daniel.d@amicosolutions.com>
2024-06-20 14:03:15 +0530

    init commit

---
+++ src/app.js
@@ -0,0 +1,108 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+const { hasSubscribers } = require('diagnostics_channel')
+const app = express()
+
+//define paths for express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//setup handlebars engines and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+
+//setup static dictionary to serve
+app.use(express.static(publicDirectoryPath))
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Daniel Ashik'
+    })
+})
+
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Daniel Ashik'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text',
+        title: 'Help',
+        name: 'Daniel Ashik'
+    })
+})
+
+
+
+app.get('/Weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an adress'
+        })
+    }
+geocode(req.query.address,(error,{latitude,longitude,location}={})=>{
+    if(error){
+        return res.send({error})
+    }
+    forecast(latitude,longitude,(error,forecastData)=>{
+        if(error){
+        return res.send({error})
+        }
+        res.send({
+
+            forecast:forecastData,
+            location,
+            address:req.query.address
+        })
+    })
+})
+
+
+
+    // res.send({
+    //     forecast:'It is Summer Season',
+    //     location:'France',
+    //     address:req.query.address
+    // })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search tearm'
+        })
+    }
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
+})
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Daniel Ashik',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Daniel Ashik',
+        errorMessage: 'Page not found.'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server port is up on 3000.')
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request=require('request')
+const forecast=(latitude,longitude,callback)=>{
+
+    const url='https://api.openweathermap.org/data/2.5/weather?lat='+latitude+'&lon='+longitude+'&appid=1fcfc5ae4f0d2e2fc8d0735a06703087'
+    // const url=`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=1fcfc5ae4f0d2e2fc8d0735a06703087`
+    request({url,json:true},(error,{body})=>{
+        if(error){
+            callback('Unable to find connection',undefined)
+        }else if(body.error){
+            callback('Unable to find the Location',undefined)
+        }else{
+            // console.log(response.body)
+            callback(undefined,
+                'It is currently: '+body.main.temp+'Minimum temperature:'+body.main.temp_min+'Maximum temperature:'+body.main.temp_max)
+            
+        }
+    })
+}
+module.exports=forecast
